6fe79c4a5efab57ec8d7ccc7b20e2d857dd909d7
HraBBriy <khr.kmx@gmail.com>
2019-10-16 18:14:13 +0300

    1. Тесты доработанны

--- src/test/java/TrianglesTest.java
+++ src/test/java/TrianglesTest.java
@@ -1,18 +1,27 @@
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import java.util.ArrayList;
 
 public class TrianglesTest {
+    private static ArrayList<int[]> triangles;
+
+    @BeforeClass
+    public static void createArray(){
+        triangles = new ArrayList<>();
+    }
 
     @Test
     public void areaOfTriangleShouldBeTheLargest(){
-        ArrayList<int[]> triangles = new ArrayList<>();
-
         int[] coordinateOfOneTriangles1 = {-1, -1, 10, -1, -1, 10};
         int[] coordinateOfOneTriangles2 = {0, 0, 1, 0, 0, 1};
+        int[] coordinateOfOneTriangles3 = {-2, 2, 4, 2, 1, 6};
+        int[] coordinateOfOneTriangles4 = {10, 22, -3, 7, 199, 200};
 
         triangles.add(coordinateOfOneTriangles1);
         triangles.add(coordinateOfOneTriangles2);
+        triangles.add(coordinateOfOneTriangles3);
+        triangles.add(coordinateOfOneTriangles4);
 
         Assert.assertEquals(coordinateOfOneTriangles1, Triangles.AreaOfTheLargestIsoscelesTriangle(triangles));
     }
@@ -21,9 +30,15 @@ public class TrianglesTest {
     public void areaOfTheLargestIsoscelesTriangleShouldHaveNull(){
         ArrayList<int[]> triangles = new ArrayList<>();
 
-        int[] coordinateOfOneTriangles = {0, 0, 0, 0, 0, 0};
+        int[] coordinateOfOneTriangles1 = {0, 0, 0, 0, 0, 0};
+        int[] coordinateOfOneTriangles2 = {-1, 1, 0, 0, 0, 0};
+        int[] coordinateOfOneTriangles3 = {-1, 1, -1, 1, -1, 1};
+        int[] coordinateOfOneTriangles4 = {10, 22, -3, 7, 199, 200};
 
-        triangles.add(coordinateOfOneTriangles);
+        triangles.add(coordinateOfOneTriangles1);
+        triangles.add(coordinateOfOneTriangles2);
+        triangles.add(coordinateOfOneTriangles3);
+        triangles.add(coordinateOfOneTriangles4);
 
         Assert.assertNull(Triangles.AreaOfTheLargestIsoscelesTriangle(triangles));
     }
