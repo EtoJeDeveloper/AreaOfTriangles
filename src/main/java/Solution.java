7afc5fe4b742c8958af4acaa2b98df47f9ddb1c5
HraBBriy <khr.kmx@gmail.com>
2019-10-17 20:50:32 +0300

    Доработка последней версии. Добавленна работа с аргументами командной строки.

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,7 +3,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class AreaOfTriangle {
+public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
